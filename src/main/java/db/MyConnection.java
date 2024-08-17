71bc00a5f72c3110793e6dc36619faa5df060bbe
likhithrajnanneboyina <likhithraj005@gmail.com>
2024-08-17 12:58:14 +0530

    first commit

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,34 @@
+package db;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/FileHiderProject?useSSL=false", "root", "SreeRama@005");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Connection ho gya saab");
+        return connection;
+    }
+    public static void closeConnection(){
+        if(connection != null){
+            try {
+                connection.close();
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+//    public static void main(String[] args) {
+//        MyConnection.getConnection();
+//    }
+}
